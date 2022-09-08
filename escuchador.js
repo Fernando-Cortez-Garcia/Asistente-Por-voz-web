9f3268875e33767414ea36dfdc7c809ecae3af48
Fernando Cortez <fernandolcortezlgarcia@gmail.com>
2022-09-07 21:21:29 -0500

    agregando boostrap

--- escuchador.js
+++ escuchador.js
@@ -5,17 +5,17 @@ recoding.interimResults = true;
 recoding.onresult = function(event) {
     const result = event.results[event.resultIndex];
     if (result.isFinal) {
+        //agregar valor al input
+        document.getElementById("text").value = result[0].transcript;
         areaText.innerHTML = result[0].transcript;
         window.open(`https://www.google.com/search?q=${result[0].transcript}`);
-
-
     }
 };
 
 let areaText = document.getElementById('text');
 areaText.addEventListener('click', function() {
     recoding.start();
-    areaText.innerHTML = 'Escuchando...';
+    areaText.placeholder = 'Escuchando...';
 });
 
 function abriryotube() {
