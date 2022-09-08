3cf2eb626be5d8a9ccfd113abc592cfaed314519
Fernando Cortez <fernandolcortezlgarcia@gmail.com>
2022-09-07 22:46:45 -0500

    Agregando buscador de wikipedia

--- escuchador.js
+++ escuchador.js
@@ -1,24 +1,30 @@
 const recoding = new webkitSpeechRecognition();
 recoding.lang = 'es-ES';
-recoding.continuous = true;
+recoding.continuous = false;
 recoding.interimResults = true;
 recoding.onresult = function(event) {
     const result = event.results[event.resultIndex];
     if (result.isFinal) {
         //agregar valor al input
         document.getElementById("text").value = result[0].transcript;
-        areaText.innerHTML = result[0].transcript;
-        window.open(`https://www.google.com/search?q=${result[0].transcript}`);
+        //abrir wikipedia con el resultado y agregarlo al inframe
+        document.getElementById("iframe").src = "https://es.wikipedia.org/wiki/" + result[0].transcript;
+
+
     }
 };
 
 let areaText = document.getElementById('text');
 areaText.addEventListener('click', function() {
+    limpiarinput();
     recoding.start();
     areaText.placeholder = 'Escuchando...';
 });
 
-function abriryotube() {
-    window.open("+");
-    //www.youtube.com/embed/
+function limpiarinput() {
+    document.getElementById("text").value = "";
+}
+
+function abrirMaps() {
+
 }
